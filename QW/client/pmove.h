741ea57ed27cfedb98c23abdc66c94ef299aa87a
Kevin Shanahan <kmshanah@disenchant.net>
2013-03-21 19:52:10 +1030

    pmove: remove unused pmplane struct
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- QW/client/pmove.h
+++ QW/client/pmove.h
@@ -28,11 +28,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "d_iface.h"
 #endif
 
-typedef struct {
-    vec3_t normal;
-    float dist;
-} pmplane_t;
-
 #define	MAX_PHYSENTS	32
 typedef struct {
     vec3_t origin;
