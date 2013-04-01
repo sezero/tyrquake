4ce6ef5b81bd1416481b7b8220525b4ba2d20368
Kevin Shanahan <kmshanah@disenchant.net>
2013-04-01 17:57:17 +1030

    windows: fix startup dialog/splash screens
    Fix the missing info in winquake.rc which I commented out long ago when I
    didn't understand the build failures. Rip out all the extra "APSTUDIO"
    generated stuff (is that an old Visual Studio?) and use the readable names
    instead of numbers wherever possible.
    
    For reasons I don't understand yet, this also fixed the audio problems
    with the windows sound driver as well... may some graphic resource needs
    to be loaded before init of sound?
    
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- QW/client/resource.h
+++ QW/client/resource.h
@@ -1,21 +1,31 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by winquake.rc
-//
-#define IDS_STRING1                     1
-#define IDI_ICON2                       1
-#define IDD_DIALOG1                     108
-#define IDD_PROGRESS                    109
-#define IDB_QWBITMAP                    112
-#define IDC_PROGRESS                    1000
+/*
+Copyright (C) 1996-1997 Id Software, Inc.
 
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        115
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+/*
+ * Windows dialog resource defines
+ * used by winquake.rc
+ */
+#define IDI_ICON2       1
+#define IDD_DIALOG1   108
+#define IDD_PROGRESS  109
+#define IDB_QWBITMAP  112
+#define IDC_PROGRESS 1000
+
+#define IDS_PALETTE_MSG "Creating 15 bit inverse palette.  This is only done once, so just be patient for the next 30-60 seconds."
