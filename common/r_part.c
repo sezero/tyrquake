7e93b69283f8bb661d3b59b9457eb1c43223933d
Ozkan Sezer <sezeroz@gmail.com>
2013-04-04 11:23:18 +1030

    particles: fix undefined behaviour in R_EntityParticles loop
    Thanks to a -Waggressive-loop-optimizations warning from gcc-4.8:
    
    common/r_part.c: In function 'R_EntityParticles':
    common/r_part.c:102:24: warning: iteration 3u invokes undefined behavior [-Waggressive-loop-optimizations]
          avelocities[0][i] = (rand() & 255) * 0.01;
                            ^
    common/r_part.c:101:2: note: containing loop
      for (i = 0; i < NUMVERTEXNORMALS * 3; i++)
      ^
    
    Signed-off-by: Ozkan Sezer <sezero@users.sourceforge.net>

--- common/r_part.c
+++ common/r_part.c
@@ -98,8 +98,11 @@ R_EntityParticles(const entity_t *ent)
     float dist = 64;
 
     if (!avelocities[0][0]) {
-	for (i = 0; i < NUMVERTEXNORMALS * 3; i++)
-	    avelocities[0][i] = (rand() & 255) * 0.01;
+	for (i = 0; i < NUMVERTEXNORMALS; i++) {
+	    avelocities[i][0] = (rand() & 255) * 0.01;
+	    avelocities[i][1] = (rand() & 255) * 0.01;
+	    avelocities[i][2] = (rand() & 255) * 0.01;
+	}
     }
 
     for (i = 0; i < NUMVERTEXNORMALS; i++) {
