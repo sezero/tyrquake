1b24539c5f8ee87e17a10f53df88970891bf4f4b
Kevin Shanahan <kevin@shanahan.id.au>
2021-07-06 06:58:32 +0930

    sound: get rid of the extra intermediate buffer in the sdl target

--- common/snd_sdl.c
+++ common/snd_sdl.c
@@ -17,6 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#include <assert.h>
 #include <stdio.h>
 
 #include "SDL.h"
@@ -35,14 +36,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 static dma_t the_shm;
 static int snd_inited;
-
-/* must not be modified while sound system is running */
-static unsigned sdl_buflen;
-
-/* must only be accessed between SDL_LockAudio() and SDL_UnlockAudio() */
-static unsigned char *sdl_buf;
-static unsigned rpos;
-static unsigned wpos;
+static int sdl_read_index;
 
 /*
  * paint_audio()
@@ -55,15 +49,19 @@ static unsigned wpos;
 static void
 paint_audio(void *unused, Uint8 *stream, int len)
 {
-    while (rpos + len > sdl_buflen) {
-	memcpy(stream, sdl_buf + rpos, sdl_buflen - rpos);
-	stream += sdl_buflen - rpos;
-	len -= sdl_buflen - rpos;
-	rpos = 0;
+    assert(!(len % (shm->channels * shm->samplebits)));
+
+    int read_buffer_size = shm->samples * (shm->samplebits / 8);
+    while (sdl_read_index + len > read_buffer_size) {
+        int copy_size = read_buffer_size - sdl_read_index;
+        memcpy(stream, shm->buffer + sdl_read_index, copy_size);
+        stream += copy_size;
+        len -= copy_size;
+        sdl_read_index = 0;
     }
     if (len) {
-	memcpy(stream, sdl_buf + rpos, len);
-	rpos += len;
+        memcpy(stream, shm->buffer + sdl_read_index, len);
+        sdl_read_index = (sdl_read_index + len) & (read_buffer_size - 1);
     }
 }
 
@@ -116,7 +114,7 @@ SNDDMA_Init(void)
 
     /*
      * Selecting the nearest power of two samples, just under half the
-     * mixahead buffer seems about right.  This is ~50ms by default
+     * mixahead buffer seems about right.  This is ~50ms by default.
      */
     desired.samples = 1 << Q_log2(desired.freq * _snd_mixahead.value * 0.5f);
     desired.callback = paint_audio;
@@ -169,28 +167,12 @@ SNDDMA_Init(void)
     shm->samplepos = 0;
     shm->submission_chunk = 1;
 
-    /*
-     * Allow enough SHM buffer to mix ahead twice the "hardware"
-     * buffer size.  This will be approximately _snd_mixahead seconds
-     * of audio.
-     *
-     * FIXME: I think this is one too many levels of buffer.  Surely
-     * paintbuffer can go straight into the hardware buffer via the
-     * callback??
-     */
-    shm->samples = obtained.samples * obtained.channels * 2;
-    sdl_buflen = shm->samples * (shm->samplebits / 8);
+    /* Allow enough SHM buffer to mix ahead up to 0.5 seconds. */
+    shm->samples = 1 << Q_log2(shm->speed * shm->channels / 2);
 
-    shm->buffer = Hunk_AllocName(sdl_buflen, "shm->buffer");
-    sdl_buf = Hunk_AllocName(sdl_buflen, "sdl_buf");
-
-    memset(shm->buffer, obtained.silence, sdl_buflen);
-    memset(sdl_buf, obtained.silence, sdl_buflen);
-
-    if (!shm->buffer || !sdl_buf)
-	Sys_Error("%s: Failed to allocate buffer for sound!", __func__);
-
-    rpos = wpos = 0;
+    int buffer_size = shm->samples * (shm->samplebits / 8);
+    shm->buffer = Hunk_AllocName(buffer_size, "shm->buffer");
+    memset(shm->buffer, obtained.silence, buffer_size);
     snd_inited = 1;
 
     /* FIXME - hack because sys_win does this differently */
@@ -208,7 +190,7 @@ SNDDMA_GetDMAPos(void)
 	return 0;
 
     SDL_LockAudio();
-    shm->samplepos = rpos / (shm->samplebits / 8);
+    shm->samplepos = sdl_read_index / (shm->samplebits / 8);
     SDL_UnlockAudio();
 
     return shm->samplepos;
@@ -232,32 +214,6 @@ Send sound to device if buffer isn't really the dma buffer
 */
 void SNDDMA_Submit(void)
 {
-    static unsigned old_paintedtime;
-    unsigned len;
-
-    if (!snd_inited || snd_blocked)
-	return;
-
-    SDL_LockAudio();
-
-    if (paintedtime < old_paintedtime)
-	old_paintedtime = 0;
-
-    len = paintedtime - old_paintedtime;
-    len *= shm->channels * (shm->samplebits / 8);
-    old_paintedtime = paintedtime;
-
-    while (wpos + len > sdl_buflen) {
-	memcpy(sdl_buf + wpos, shm->buffer + wpos, sdl_buflen - wpos);
-	len -= sdl_buflen - wpos;
-	wpos = 0;
-    }
-    if (len) {
-	memcpy(sdl_buf + wpos, shm->buffer + wpos, len);
-	wpos += len;
-    }
-
-    SDL_UnlockAudio();
 }
 
 void
@@ -276,8 +232,15 @@ S_UnblockSound(void)
 	SDL_PauseAudio(0);
 }
 
-/*
- * shm->buffer is not the real DMA buffer, so no locking needed here
- */
-int SNDDMA_LockBuffer(void) { return 0; }
-void SNDDMA_UnlockBuffer(void) { }
+int
+SNDDMA_LockBuffer()
+{
+    SDL_LockAudio();
+    return 0;
+}
+
+void
+SNDDMA_UnlockBuffer()
+{
+    SDL_UnlockAudio();
+}
